85d335a38440e5ece7cceca960a76812b033af66
= <=>
2023-03-03 15:14:41 -0800

    Second initial state

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db')
 
 // Load config vars
 dotenv.config({ path: './config/config.env' })
 
+// Connect to database
+connectDB()
+
 const app = express()
+
+// Dev logging midleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// define routes
+app.use('/api/v1/bootcamps', require('./routes/bootcamps'))
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
+
+// Handle unhandle promise rejections
+process.on('unhandledRejections', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close server and exit process
+  server.close(() => process.exit(1))
+})
